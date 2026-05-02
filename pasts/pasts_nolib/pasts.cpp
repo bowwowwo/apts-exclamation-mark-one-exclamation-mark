0b1bc16e463785aa20d4baf2d6071acf166331d4
wiyhs <96704375+wiyhs@users.noreply.github.com>
2026-05-02 14:19:51 +0300

    i dont like this one

--- pasts/pasts_nolib/pasts.cpp
+++ pasts/pasts_nolib/pasts.cpp
@@ -7,19 +7,28 @@ class pastins{
 struct node {
     char letter;
     string* input;
+    int arrLength;
     node *next;
     node *prev;
-    node(int val) : data(val), next(nullptr), prev(nullptr) {}
+    node(int val, string* val2, int val3) : letter(val), input(val2), arrLength(val3), next(nullptr), prev(nullptr) {}
+
+    ~node() {
+        delete[] input;
+    }
 };
 string* arr;
+node* head;
+node* tail;
 
 public:
     pastins();
     ~pastins(){
         delete [] arr;
+        deleteLlist();
+
     }
     void CreateArray(string line){
-        arrSize = getWordNr(line);
+        int arrSize = getWordNr(line);
         arr = new string[arrSize];
 
         string word = "";
@@ -37,7 +46,7 @@ public:
         }
         if (!word.empty()) {
             arr[index++] = word;
-    }
+        }
     }
     string tolower(string s){
         int val = 'a' - 'A';
@@ -67,27 +76,83 @@ public:
         }
         return count;
     }
-    void charLinkedList(){
+    void charLinkedList() {
         string prev = "";
         char c;
-        if
-        for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-            c = getFirstChar(arr[i]);
-            for(char d : prev) if(d == c) continue;
+        int count;
+        string* arr2;
 
-            //create nodes for each new char
+        head = nullptr;
+        node* last = nullptr;
+
+        for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+            c = getFirstChar(arr[i]);
+
+            bool exists = false;
+            for (char d : prev) {
+                if (d == c) {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (exists) continue;
+
+            count = countArraywords(c);
+            arr2 = new string[count];
+
+            int k = 0;
+            for(int j = 0; j < sizeof(arr)/sizeof(arr[0]); j++){
+                if(c == getFirstChar(arr[j])){
+                    arr2[k] = arr[j];
+                    k++;
+                }
+            }
+
+
+            node* p = new node(c, arr2, count);
+
+            if (head == nullptr) {
+                head = last = p;
+            }
+            else {
+                last->next = p;
+                p->prev = last;
+                last = p;
+            }
 
             prev += c;
         }
     }
+    int countArraywords(char c){
+        int count = 0;
+        char c1;
+        for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+            c1 = getFirstChar(arr[i]);
+            if(c == c1) count ++;
+        }
+
+        return count;
+    }
+
     string* sortAlphabet();
     string* formatOutput();
+
+    void deleteLlist(){
+        node* current = head;
+
+        while (current != nullptr) {
+            node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+
+        head = nullptr;
+    }
 };
 
 
 int main(){
-
-    pastins p;
 
     string fName = "post.in.txt";
     string line;
@@ -100,9 +165,9 @@ int main(){
         return 1;
     }
 
-    while (getline(fin, line)) {
-        fout << p.CreateInput(line) << endl;
-    }
+    //while (getline(fin, line)) {
+       // fout << (line) << endl;
+    //}
 
     fin.close();
     fout.close();
